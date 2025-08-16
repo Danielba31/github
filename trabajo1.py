8bf4bb7e76d744f91f095108cea785d460aed74b
Daniel <danieldicelis@gmail.com>
2025-08-16 13:37:14 -0500

    primer commit

---
+++ trabajo1.py
@@ -0,0 +1,6 @@
+print("bienvenido al programa de prueba para github")
+suma=0
+for i in range(1, 11):
+    suma += i
+print("La suma de los primeros 10 números es:", suma)
+print("Fin del programa")
